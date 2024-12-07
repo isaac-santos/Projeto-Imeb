c6dcdf0120a6810d3eb03a602421520d57a9d847
isaac-santos <isaacconceicao26@gmail.com>
2024-12-07 20:50:13 -0300

    atualizei o estilo do Projeto-Imeb

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 function toggleMenu() {
     const nav = document.querySelector('nav');
     const hamburger = document.querySelector('.hamburger');
+    const logo = document.querySelector('.logo');
     if (nav && hamburger) {
         
         nav.classList.toggle('abrir');
         
         
         hamburger.classList.toggle('ativo');
+
+        logo.classList.toggle('ativo');
     }
 }
