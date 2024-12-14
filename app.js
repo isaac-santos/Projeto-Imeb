4c15376e1c803270afe154177a3cea90dd6a6cbf
isaac-santos <isaacconceicao26@gmail.com>
2024-12-14 00:18:48 -0300

    atualizei o projeto-imeb

--- app.js
+++ app.js
@@ -14,3 +14,35 @@ function toggleMenu() {
         logo.classList.toggle('ativo');
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        loop: true, // Ativar o loop dos slides
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: true,
+            pauseOnMouseEnter: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+    });
+
+    document.body.addEventListener('touchstart', () => {
+        swiper.autoplay.start();
+    });
+});
+
